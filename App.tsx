b7151adf5850dcdf61ddaf7f31e3d5d55475735a
BlueBiuBiu <1910916141@qq.com>
2024-03-05 10:43:52 +0800

    feat: 集成了React Native Elements

--- App.tsx
+++ App.tsx
@@ -14,38 +14,52 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {enableScreens} from 'react-native-screens';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import {ThemeProvider, createTheme} from '@rneui/themed';
 
 import Login from './src/views/Login/Login';
 import Tabbar from './src/views/Tabbar/Tabbar';
+
+// 定制主题
+const theme = createTheme({
+  lightColors: {
+    primary: '#e7e7e8',
+  },
+  darkColors: {
+    primary: '#000',
+  },
+  mode: 'light',
+});
 
 enableScreens();
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <SafeAreaProvider style={styles.root}>
-        <StatusBar barStyle={'dark-content'} backgroundColor={'#ffffff'} />
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Login"
-            screenOptions={{
-              ...TransitionPresets.SlideFromRightIOS,
-            }}>
-            <Stack.Screen
-              name="Login"
-              component={Login}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="Tabbar"
-              component={Tabbar}
-              options={{headerShown: false}}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <SafeAreaProvider style={styles.root}>
+          <StatusBar barStyle={'dark-content'} backgroundColor={'#ffffff'} />
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Login"
+              screenOptions={{
+                ...TransitionPresets.SlideFromRightIOS,
+              }}>
+              <Stack.Screen
+                name="Login"
+                component={Login}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Tabbar"
+                component={Tabbar}
+                options={{headerShown: false}}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
--- src/views/Tabbar/Home/Home.tsx
+++ src/views/Tabbar/Home/Home.tsx
@@ -2,8 +2,11 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 import {useAppSelector, shallowEqualApp} from '../../../store';
+import {Button, Icon, SpeedDial} from '@rneui/themed';
 
 const Home = () => {
+  const [open, setOpen] = React.useState(false);
+
   const {userInfo} = useAppSelector(
     state => ({
       userInfo: state.userInfo.userInfo,
@@ -15,6 +18,27 @@ const Home = () => {
     <View>
       <Text>{userInfo.phone}</Text>
       <Text>{userInfo.password}</Text>
+      <Button title="Solid" />
+      <Button title="Outline" type="outline" />
+      <Button title="Clear" type="clear" />
+      <Icon name="rowing" />
+      <SpeedDial
+        isOpen={open}
+        icon={{name: 'edit', color: '#fff'}}
+        openIcon={{name: 'close', color: '#fff'}}
+        onOpen={() => setOpen(!open)}
+        onClose={() => setOpen(!open)}>
+        <SpeedDial.Action
+          icon={{name: 'add', color: '#fff'}}
+          title="Add"
+          onPress={() => console.log('Add Something')}
+        />
+        <SpeedDial.Action
+          icon={{name: 'delete', color: '#fff'}}
+          title="Delete"
+          onPress={() => console.log('Delete Something')}
+        />
+      </SpeedDial>
     </View>
   );
 };
